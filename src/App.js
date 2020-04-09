ec3791db420772c74a99a7e86b2603729c6791b6
Dwamper <shev.ea@ya.ru>
2020-04-10 00:34:45 +0300

    make clean

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
 
 class App extends Component {
-  state = {
-    persons: [
-      { name: 'Dwamper', age: 30 },
-      { name: 'Ainvolt', age: 31 },
-    ]
-  }
-  switchName = name => {
-    this.setState({
-      persons: [
-        { name, age: 30 },
-        { name: 'Ainvolt', age: 31 },
-      ]
-    })
-  }
-
-  changeName = event => {
-
-    this.setState({
-      persons: [
-        { name: event.target.value, age: 30 },
-        { name: 'Ainvolt', age: 31 },
-      ]
-    })
-  }
-
   render() {
-    const person = this.state.persons[0]
     return (
       <div className="App">
-        <Person
-          name={person.name}
-          age={person.age}
-          atChange={this.changeName}
-          click={() => this.switchName('Egorka')}>Some text</Person>
       </div>
     );
   }
